392ad19ec52fcf097d55ed1b5308f0a3d496e4ee
svxezm <igorb.kuhl@gmail.com>
2025-11-25 12:01:49 -0300

    chore: adapt build for each available system with nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,21 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
-
-  outputs = { nixpkgs, ... }:
-  let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-  {
-    packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-      pname = "pls";
-      version = "0.1.0";
-      src = ./.;
-      cargoLock.lockFile = ./Cargo.lock;
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "pls";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+      }
+    );
 }
